15ecc273e48d942f1a06bef21b5f82cc76ed7906
skjeng <skjeng@ntu.edu.tw>
2015-04-13 21:28:46 +0800

    Completed TestScenario_1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestScenario_1()
+        {
+            int x = 3;
+            int y = 5;
+            Program.Swap(ref x, ref y);
+            Assert.IsTrue(x == 5 && y == 3);
+        }
+    }
+}
